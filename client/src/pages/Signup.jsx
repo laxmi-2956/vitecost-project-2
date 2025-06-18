1eb88c1dea42b4ccb71d167ff04aa5b6b5463f36
laxmi-2956 <156445712+laxmi-2956@users.noreply.github.com>
2025-06-18 15:43:17 +0530

    Update Signup.jsx

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -9,6 +9,7 @@ import { signupError, signupStart, signupSuccess } from "../redux/auth/authSlice
 
 
 
+
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
